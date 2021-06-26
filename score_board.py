2055bdeaf75612be05114fedd2a7d27c9e081eac
rakam Singh <83465261+rakam1711@users.noreply.github.com>
2021-06-26 13:48:00 -0200

    Create score_board.py

---
+++ score_board.py
@@ -0,0 +1,28 @@
+# snake_game_python
+from turtle import Turtle
+FONT = ('arial', 25, 'bold')
+
+
+class Score(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.color('green')
+
+        self.penup()
+        self.goto(0, 270)
+        self.hideturtle()
+        self.update_score()
+
+    def update_score(self):
+        self.write(f"score: {self.score}", align='center', font=FONT)
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.color('red')
+        self.write("GAME OVER", align='center', font=FONT)
+
+    def increase_score(self):
+        self.score += 1
+        self.clear()
+        self.update_score()
